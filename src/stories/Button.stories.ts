cab1e87ab3f794811f95bc94d403a3e8041cc5b1
Sebastian Coates <sebastiancoates@Sebastians-MacBook-Pro.local>
2025-06-09 13:54:44 -0400

    update dependencies, remove default examples, and include MUI Button example

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -16,7 +16,7 @@ const meta = {
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    color: { control: 'color' },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
@@ -28,27 +28,41 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: 'Button',
+    color: 'primary',
+    children: 'Button',
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: 'Button',
+    color: 'secondary',
+    children: 'Button',
   },
 };
 
 export const Large: Story = {
   args: {
     size: 'large',
-    label: 'Button',
+    children: 'Button',
   },
 };
 
 export const Small: Story = {
   args: {
     size: 'small',
-    label: 'Button',
+    children: 'Button',
+  },
+};
+
+export const Contained: Story = {
+  args: {
+    variant: 'contained',
+    children: 'Button',
+  },
+};
+export const Outlined: Story = {
+  args: {
+    variant: 'outlined',
+    children: 'Button',
   },
 };
