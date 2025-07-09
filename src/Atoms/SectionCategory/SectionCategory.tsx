5ddbfb76ba5aa549ed089481216fff0e204c3f63
derrysukrie <derrysukrie@gmail.com>
2025-07-09 07:28:30 +0700

    ♻️ (SectionCategory.tsx): remove unused state and commented code to clean up the component and improve readability

--- src/Atoms/SectionCategory/SectionCategory.tsx
+++ src/Atoms/SectionCategory/SectionCategory.tsx
@@ -14,7 +14,6 @@ interface CustomAccordionProps {
 
 export default function CustomAccordion({ title = "Tagged as", children, expanded = false, selected = false }: CustomAccordionProps) {
   const [expandedState, setExpandedState] = useState(expanded);
-  //   const [selectValue, setSelectValue] = useState("");
 
   const handleChange = () => {
     setExpandedState(!expandedState);
@@ -26,7 +25,6 @@ export default function CustomAccordion({ title = "Tagged as", children, expande
       onChange={handleChange}
       elevation={0}
       sx={{
-        // borderBottom: selected ? "none" : "1px solid #E2E8F0",
         backgroundColor: selected ? "#E2E8F0" : "transparent",
         borderRadius: "0 !important",
         "&:before": {
@@ -44,17 +42,6 @@ export default function CustomAccordion({ title = "Tagged as", children, expande
         "&:last-of-type": {
           borderRadius: "0 !important",
         },
-        // "&:hover": {
-        //   outline: "none",
-        //   borderColor: "white"
-        // },
-        // "&.Mui-focused": {
-        //   outline: "none",
-        //   borderColor: "white"
-        // },
-        // "&.Mui-expanded": {
-        //   backgroundColor: "#E2E8F0"
-        // }
       }}
     >
       <AccordionSummary
@@ -111,28 +98,6 @@ export default function CustomAccordion({ title = "Tagged as", children, expande
         </Box>
       </AccordionSummary>
       <AccordionDetails>
-        {/* <List sx={{ p: 0 }}>
-          {selectOptions.map((option) => (
-            <ListItem key={option.value} disablePadding>
-              <ListItemButton
-                selected={selectValue === option.value}
-                onClick={() => {
-                    setSelectValue(option.value);
-                    setExpanded(false);
-                }}
-                sx={{ 
-                  borderRadius: 1, 
-                  mb: 0.5,
-                  "&.Mui-selected": {
-                    backgroundColor: "rgba(0, 0, 0, 0.04)"
-                  }
-                }}
-              >
-                <ListItemText primary={option.label} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List> */}
         {children}
       </AccordionDetails>
     </Accordion>
