4ad05ccd3e13e6c5dd3925e5ff5fc23f6a698d6d
derrysukrie <derrysukrie@gmail.com>
2025-07-14 00:07:42 +0700

    ♻️ (Message components): remove unused IconButton import from MessageImage and MessageLink components to clean up code ♻️ (MessageScheduled): remove unused AccessTime import to streamline component ♻️ (MMS stories): remove unused reactions import to simplify the story file

--- src/Molecules/Message/MessageItem/components/MessageScheduled/MessageScheduled.tsx
+++ src/Molecules/Message/MessageItem/components/MessageScheduled/MessageScheduled.tsx
@@ -1,5 +1,4 @@
 import { Avatar, Box, Stack, Typography } from "@mui/material";
-import AccessTime from "@mui/icons-material/AccessTime";
 import type { MessageType, Reaction } from "../../../types";
 import { Reactions } from "../Reactions/Reactions";
 import {
