828ab2968f088f87ed2a1bc1874892bbcd251e7a
Derry <derrysukrie@gmail.com>
2025-07-20 21:46:19 +0700

    ✨ (Description.tsx): update Typography color to primary.light for better visibility and aesthetics

--- src/Organism/SentForm/Steps/Description.tsx
+++ src/Organism/SentForm/Steps/Description.tsx
@@ -4,7 +4,7 @@ export const Description = () => {
   return (
     <Box height={"100%"} display={"flex"} flexDirection={"column"} justifyContent={"center"}>
       <Box px="24px">
-        <Typography variant="h3" fontWeight={500}>
+        <Typography variant="h3" color={"primary.light"} fontWeight={500}>
           Great, we have a variety of plans to fit your needs. Lets start with
           some questions about you, after that we’ll the plan thats a perfect
           fit!
